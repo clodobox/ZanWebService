ec9f24783b08a7db1bd51af1a2ae18f07fdfad75
Julien Lanza <julien.lanza@ik.me>
2023-05-01 13:02:33 +0300

    Update code

--- app.py
+++ app.py
@@ -14,13 +14,12 @@ LOGS_DIR = 'logs'
 server_instances = {}
 launched_servers = {}
 server_creations = {}
-next_available_port = 10650
-last_available_port = 10700
+next_available_port = int(os.environ['next_available_port'])
+last_available_port = int(os.environ['last_available_port'])
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
-# Replace this with the path to your Zandronum server executable
-zandronum_server_path = '/usr/games/zandronum/zandronum-server'
+zandronum_server_path = os.environ['zandronum_server_path']
 
-# Set flags
+# Set Zandronum flags
 dmflags = [
     {"value": 1, "description": "Disallow health (In Deathmatch)"},
     {"value": 2, "description": "Disallow powerups (In Deathmatch)"},
@@ -322,4 +321,3 @@ def get_last_connected_timestamp(log_file):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6556, debug=True)
-
